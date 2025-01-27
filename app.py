392a97f2199a64775c7d2a24ff4f782700830c31
21f3002920 <21f3002920@ds.study.iitm.ac.in>
2025-01-27 22:08:48 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from backend.config import LocalDevelopmentConfig
+from backend.models import db, User, Role
+from flask_security import Security, SQLAlchemyUserDatastore
+
+def createApp():
+    app=Flask(__name__)
+    app.config.from_object(LocalDevelopmentConfig)
+    db.init_app(app)
+    
+    #flask security
+    datastore=SQLAlchemyUserDatastore(db, User, Role)
+    app.security=Security(app, datastore=datastore)
+    app.app_context().push()
+
+    return app
+
+app=createApp()
+
+@app.get('/')
+def home():
+    return '<h1> home page </h1>'
+
+if(__name__=='__main__'):
+    app.run()
