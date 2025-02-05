abfc14db0ab5854e8915d692278a149899b9174a
21f3002920 <21f3002920@ds.study.iitm.ac.in>
2025-02-05 18:29:47 +0530

    Created database schema for users, services, and service requests

--- backend/models.py
+++ backend/models.py
@@ -5,6 +5,7 @@ db=SQLAlchemy()
 
 #Database Schema for Users
 class User(db.Model,UserMixin):
+    __tablename__ = 'user'
     id=db.Column(db.Integer, primary_key=True)
     email=db.Column(db.String, unique=True, nullable=False)
     password=db.Column(db.String, nullable=False)
@@ -23,3 +24,52 @@ class UserRoles(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     user_id=db.Column(db.Integer, db.ForeignKey('user.id'))
     role_id=db.Column(db.Integer, db.ForeignKey('role.id'))
+
+class Service(db.Model):
+    __tablename__='service'
+    service_id=db.Column(db.Integer, primary_key=True)
+    service_name=db.Column(db.String,nullable=False)
+    service_baseprice=db.Column(db.Integer, nullable=False)
+    service_time=db.Column(db.Integer, nullable=False)
+    service_description=db.Column(db.String,nullable=False)
+
+
+
+
+
+
+
+
+
+
+
+class ServiceRequest(db.Model):
+    __tablename__='servicerequest'
+    service_request_id=db.Column(db.Integer, primary_key=True)
+    service_id=db.Column(db.Integer, db.ForeignKey('service.id'), nullable=False)
+    customer_id=db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    professional_id=db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date_of_request=db.Column(db.Date, nullable=True)
+    date_of_completion=db.Column(db.Date, nullable=True)
+    service_status=db.Column(db.String, nullable=False)
+
+class Customer(db.Model):
+    __tablename__='customer'
+    c_id=db.Column(db.Integer, primary_key=True)
+    c_userid=db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    c_flag=db.Column(db.Integer, nullable=False)
+    c_address=db.Column(db.String, nullable=False)
+    c_pincode=db.Column(db.Integer, nullable=False)
+    c_phone=db.Column(db.Integer, nullable=False)
+
+class Professional(db.Model):
+    __tablename__='professional'
+    p_id = db.Column(db.Integer, primary_key=True)
+    p_userid=db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    p_serviceid=db.Column(db.Integer, db.ForeignKey('service.id'), nullable=False)
+    p_experience=db.Column(db.Integer, nullable=False)
+    p_flag=db.Column(db.Integer, nullable=False)
+    p_duty=db.Column(db.String, nullable=False)
+    p_aadhaarnumber=db.Column(db.Integer, nullable=False)
+    p_phone=db.Column(db.Integer, nullable=False)
+    p_pincode=db.Column(db.Integer, nullable=False)
