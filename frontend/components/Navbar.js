3807727641b69b4724a43077bda5eaae4cf6bd83
21f3002920 <21f3002920@ds.study.iitm.ac.in>
2025-03-02 20:31:31 +0530

    Navigation bar routing improved for better navigation within application.

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,14 +1,28 @@
 export default {
     template:`
         <div class="header">
-            <router-link to="/" class="navbarleft">Servico</router-link>
+            <router-link :to="goBackPath" class="navbarleft">Servico</router-link>
             <button class="logout" v-if="$store.state.loggedIn" @click="$store.commit('logout'),$router.push('/')">Logout</button>
         </div>
         
 
     `,
+    computed:{
+        goBackPath(){
+            if (this.$store.state.role === "Admin") {
+                return ("/Admindashboard")
+            }
+            else if (this.$store.state.role === "Customer") {
+                return ("/Customerdashboard")
+            }
+            else if (this.$store.state.role === "Professional") {
+                return ("/Professionaldashboard")
+            }
+            else{
+                return("/")
+            }
     
-  };
+  }}};
   
     
 
